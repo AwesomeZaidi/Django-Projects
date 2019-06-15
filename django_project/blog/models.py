b31cb5522f5d2ff035056b36ee8da84f2ae58db1
Asim Zaidi <asimzaidih@gmail.com>
2019-06-15 15:00:06 -0700

    My first django db query 🤘

--- django_project/blog/models.py
+++ django_project/blog/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
+
 
 # Create your models here.
 class Post(models.Model):
@@ -8,3 +10,8 @@ class Post(models.Model):
     # last_modified = models.DateTimeField(auto_now=True)
     # date_posted = models.DateTimeField(auto_now_add==True)
     date_posted = models.DateTimeField(default=timezone.now)
+    # CASCADE: if a user is deleted, we want to deleted this associated Post as well.
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,24 +1,9 @@
 from django.shortcuts import render
-# from django.http import HttpResponse
-
-posts = [
-    {
-        'author': 'CoreyMS',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'Aug 28, 2019'
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'Jan 12, 2019',
-    }
-]
+from .models import Post
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
