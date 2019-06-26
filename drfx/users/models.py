d075965ed7403ef80ce387a48f471560bda0ec43
Asim Zaidi <asimzaidih@gmail.com>
2019-06-26 14:55:33 -0700

    Created User modela and admin

--- drfx/users/models.py
+++ drfx/users/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser 
 # Create your models here.
+
+class CustomUser(AbstractUser):
+    name = models.CharField(blank=True, max_length=46)
+
+    def __str__(self):
+        return self.email
+        
