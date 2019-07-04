023176c96d26d2a742a000808461160a4c23c11e
Asim Zaidi <asimzaidih@gmail.com>
2019-07-04 10:16:23 -0700

    Working on rest jwt app

--- drfx/drfx/settings.py
+++ drfx/drfx/settings.py
@@ -40,11 +40,17 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'rest_auth',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'rest_auth.registration',
 
     'api',
     'users',
 ]
 
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+SITE_ID = 1
 AUTH_USER_MODEL = 'users.CustomUser'
 
 MIDDLEWARE = [
